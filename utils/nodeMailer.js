74a686801648c1ec93e35a2c11358e539fd6942b
D'Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-03-26 22:15:45 +0100

    retore email verification code

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -3,17 +3,17 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: 'mail.privateemail.com',
+    host: 'server122.web-hosting.com',
     port: 465,  
     auth: {
-      user: 'verify@mobiledoctor.homes',
+      user: 'verificationcode@mobiledoctor.firststarconsults.online',
       pass: process.env.VERIFICATION_EMAIL_PASSWORD,
     },
   });
 
 export const sendVerificationEmail = async (to, code) => {
   const mailOptions = {
-    from: 'verify@mobiledoctor.homes',
+    from: 'verificationcode@mobiledoctor.firststarconsults.online',
     to,
     subject: 'Verification Code',
     text: `Your verification code is: ${code}`,
