b7b6d08f46884bcfbfaecd941d5f533f934f7319
princelee <princeleepraise@gmail.com>
2024-05-02 17:27:20 +0100

    updated updateMedicalReport function

--- controllers/medicalReportsController.js
+++ controllers/medicalReportsController.js
@@ -36,31 +36,74 @@ export const updateMedicalReport = async (req, res) => {
 export const getMedicalReport = async (req, res) => {
   try {
     const { userId } = req.params;
-    const medicalRecord = await MedicalRecord.findOne({ user: userId });
-    res.json(medicalRecord);
+    const medicalRecord = await MedicalRecord.findById(userId);
+    if (medicalRecord) {
+      res.json(medicalRecord);
+    } else {
+      res.status(404).json({ message: "Medical record not found for the specified user" });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
 
 export const uploadTestResult = async (req, res) => {
   try {
     const { userId } = req.params;
-    const { file, folderName } = req.body; // Using userId instead of medicalRecordId
-    const uploadedFile = await upload(file, folderName);
+    const { folderName } = req.body;
+    const files = req.files; // Assuming files are uploaded as an array of files
 
-    // Update the testReport field in the MedicalRecord schema
-    await MedicalRecord.findOneAndUpdate(
-      { user: userId },
-      { $push: { testResults: uploadedFile.secure_url } },
+    // Find the medical record by user ID
+    const medicalRecord = await MedicalRecord.findById(userId);
+
+    if (!medicalRecord) {
+      return res.status(404).json({ success: false, error: 'Medical record not found for the specified user' });
+    }
+
+    const updateQueries = {};
+    const uploadedFiles = [];
+
+    // Iterate over each file and upload to Cloudinary
+    for (const key in files) {
+      const file = files[key];
+      const fileTypes = ['image/jpeg', 'image/png', 'image/jpg']; // Update file types as needed
+      const imageSize = 1024; // Assuming you want to use 1 MB = 1024 KB
+
+      if (!fileTypes.includes(file.mimetype)) {
+        return res.status(400).json({ success: false, error: 'File formats supported: JPG, PNG, JPEG' });
+      }
+
+      if (file.size / 1024 > imageSize) { // Assuming size is in bytes, convert to KB
+        return res.status(400).json({ success: false, error: `File size should be less than ${imageSize}kb` });
+      }
+
+      // Upload file to Cloudinary
+      const cloudFile = await upload(file.tempFilePath, folderName);
+      uploadedFiles.push({ [key]: cloudFile.url });
+      updateQueries[`testResults.${key}`] = cloudFile.url;
+    }
+
+    // Update medical record model with the Cloudinary URLs for all uploaded files
+    const updatedMedicalRecord = await MedicalRecord.findByIdAndUpdate(
+      medicalRecord._id,
+      { $set: updateQueries },
       { new: true }
     );
 
-    res.json({ url: uploadedFile.secure_url });
+    res.status(201).json({
+      success: true,
+      message: 'Test results uploaded successfully',
+      updatedMedicalRecord: updatedMedicalRecord,
+      fileUrls: uploadedFiles,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error('Error uploading test results:', error);
+    res.status(500).json({ success: false, error: 'Error uploading test results', errorMessage: error.message });
   }
 };
+
+
 
 export const getTestResult = async (req, res) => {
     try {
