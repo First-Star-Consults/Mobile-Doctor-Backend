14a07d8ea78259a290c1aeb03a902ae5c3a8ae5a
Amazing Prince Lee <prince_lee.nna85@ymail.com>
2024-09-16 05:17:43 +0100

    remove required from content in message model

--- models/messageModel.js
+++ models/messageModel.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema({
   conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  content: { type: String, required: true },
+  content: { type: String},
   timestamp: { type: Date, default: Date.now },
   fileUrl: { type: String }, 
   seen: { type: Boolean, default: false }
